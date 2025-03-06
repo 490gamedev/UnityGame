cfb72567dd1317d7c53d58dc4ea3be25b5b60dd4
David Nguyen <huyn9@outlook.com>
2025-03-05 17:49:11 -0800

    change color, link the main menu to the character seleciton scene

--- GameDevelopment/Assets/Scripts/MainMenu.cs
+++ GameDevelopment/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame() {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void CharacterGame() {
         SceneManager.LoadSceneAsync(1);
     }
 
